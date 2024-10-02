87d2a5ce03b970458fbe796f376fcf476c990b16
TiMolod <misha.drozdov.99@bk.ru>
2024-10-02 19:26:31 +0300

    Stage 2

--- Character/player.gd
+++ Character/player.gd
@@ -1,34 +1,42 @@
 extends CharacterBody2D
 
+@export var speed : float = 200.0
 
-@export var speed : float = 300.0
-@export var jump_velocity : float = -400.0
-@export var double_jump_velocity : float = -200
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine : CharacterStateMachine = $CharacterStateMachine
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped: bool = false
+var direction : Vector2 = Vector2.ZERO
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	else:
-		has_double_jumped = false	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump"):
-		if is_on_floor():
-			velocity.y = jump_velocity
-		elif !(has_double_jumped):
-			velocity.y = double_jump_velocity
-			has_double_jumped = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * speed
+	direction = Input.get_vector("left", "right", "up", "down")
+	
+	# Control whether to move or not to move
+	if direction.x != 0 && state_machine.check_if_can_move():
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation_parameters()
+	update_facing_direction()
+	
+func update_animation_parameters():
+	animation_tree.set("parameters/Move/blend_position", direction.x)
+
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Kleh
+# Sidescroller-Quickstart-Godot-4
+Tutorial project to demo how to setup a sidescrolling character with animations, smooth camera, and simple tilemap collisions
+
+== Controls ==
+
+A - Move Left
+
+D - Move Right
+
+Space - Jump

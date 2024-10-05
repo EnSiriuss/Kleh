484bf06890b08fcf78790d086e11232bff1045bf
EnSiriuss <144539037+EnSiriuss@users.noreply.github.com>
2024-10-05 20:22:42 +0500

    Merge remote-tracking branch 'origin/main'

--- Character/Enemies/Snail/Damageble.gd
+++ Character/Enemies/Snail/Damageble.gd
@@ -1,9 +1,18 @@
 extends Node
 
-@export var health: float = 20
+class_name Damageable
 
+@export var health: float = 20
+	#get: 
+		#return health
+	#set(value):
+		#SignalBus.emit_signal("on_health_changed", get_parent(),value - health)
+		#health = value	
+	
 func got_hit(damage : int):
 	health -= damage
+	
+	emit_signal("on_hit",get_parent(), damage)
 	
 	if(health<=0):
 		get_parent().queue_free()
--- Character/sword.gd
+++ Character/sword.gd
@@ -3,7 +3,6 @@ extends Area2D
 @export var damage : int = 10
 func _on_body_entered(body):
 	for child in body.get_children():
-		if child is Damageadle :
+		if child is Damageable:
 			child.hit(damage)
-			print_debug(body.name + "took" + str(damage) + ".")
 	

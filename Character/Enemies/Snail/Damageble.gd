c912b9bb31704093f4da05d7415a54b1c00652d8
TiMolod <misha.drozdov.99@bk.ru>
2024-10-06 20:45:00 +0300

    Damage/kill

--- Character/Enemies/Snail/Damageble.gd
+++ Character/Enemies/Snail/Damageble.gd
@@ -3,21 +3,10 @@ extends Node
 class_name Damageable
 
 @export var health: float = 20
-	#get: 
-		#return health
-	#set(value):
-		#SignalBus.emit_signal("on_health_changed", get_parent(),value - health)
-		#health = value	
+
 	
 func got_hit(damage : int):
 	health -= damage
 	
-	emit_signal("on_hit",get_parent(), damage)
-	
 	if(health<=0):
 		get_parent().queue_free()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
